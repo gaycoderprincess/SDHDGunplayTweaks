32b41bee8ec985c435dcdd6a16b5609cbb4de068
💖 Hana 💖 <gaycoderprincess@proton.me>
2024-06-04 11:26:12 +0200

    update to new hooklib

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include <windows.h>
-#include <psapi.h>
 #include "toml++/toml.hpp"
 #include "nya_commonhooklib.h"
 
 #include "game.h"
-
-auto module = (uintptr_t)GetModuleHandle(nullptr);
 
 bool IsPlayerWieldingShotgun(UFG::AimingPlayerComponent* pThis) {
 	if (!pThis) return false;
@@ -22,7 +19,7 @@ bool IsPlayerWieldingShotgun(UFG::AimingPlayerComponent* pThis) {
 	return pFireMode->mSimObjectWeaponType == 8;
 }
 
-auto updateAccuracy_OrigAddress = (void(__fastcall*)(UFG::AimingPlayerComponent*, const float))(module + 0x56E9B0);
+auto updateAccuracy_OrigAddress = (void(__fastcall*)(UFG::AimingPlayerComponent*, const float))(NyaHookLib::mEXEBase + 0x56E9B0);
 void __fastcall updateAccuracyPatch(UFG::AimingPlayerComponent* pThis, const float fDeltaS) {
 	pThis->m_fOverrideReticleSize = IsPlayerWieldingShotgun(pThis) ? -1.0 : 0.0;
 	updateAccuracy_OrigAddress(pThis, fDeltaS);
@@ -35,9 +32,7 @@ BOOL WINAPI DllMain(HINSTANCE, DWORD fdwReason, LPVOID) {
 
 	switch( fdwReason ) {
 		case DLL_PROCESS_ATTACH: {
-			MODULEINFO info;
-			GetModuleInformation(GetCurrentProcess(), (HMODULE)module, &info, sizeof(info));
-			if ((uintptr_t)info.EntryPoint - (uintptr_t)info.lpBaseOfDll != 0x12BA5B4) {
+			if (NyaHookLib::GetEntryPoint() != 0x12BA5B4) {
 				MessageBoxA(nullptr, "Unsupported game version! Make sure you're using the latest Steam release (.exe size of 37490688 bytes)", "nya?!~", MB_ICONERROR);
 				return TRUE;
 			}
@@ -48,20 +43,20 @@ BOOL WINAPI DllMain(HINSTANCE, DWORD fdwReason, LPVOID) {
 			bool bNoSteerDeadzone = config["extras"]["no_steering_deadzone"].value_or(false);
 
 			if (bNoAutoAim) {
-				NyaHookLib::Patch<uint16_t>(module + 0x57880C, 0x9090); // UFG::AimingPlayerComponent::updateSoftLockOffset
+				NyaHookLib::Patch<uint16_t>(NyaHookLib::mEXEBase + 0x57880C, 0x9090); // UFG::AimingPlayerComponent::updateSoftLockOffset
 			}
 
 			if (bAccurateWeapons) {
 				// UFG::AimingPlayerComponent::updateAccuracy, unrelated but useful to already have a ptr to AimingPlayerComponent :3
-				updateAccuracy_OrigAddress = (void (*)(UFG::AimingPlayerComponent *,
-													   const float)) NyaHookLib::ReadRelative(module + 0x5590EC + 1);
-				NyaHookLib::PatchRelative(NyaHookLib::JMP, module + 0x5590EC, &updateAccuracyPatch);
+				updateAccuracy_OrigAddress = (void (__fastcall*)(UFG::AimingPlayerComponent *,
+													   const float)) NyaHookLib::ReadRelative(NyaHookLib::mEXEBase + 0x5590EC + 1);
+				NyaHookLib::PatchRelative(NyaHookLib::JMP, NyaHookLib::mEXEBase + 0x5590EC, &updateAccuracyPatch);
 			}
 
 			if (bNoSteerDeadzone) {
 				// UFG::VehicleAnalogDriverInput::calcSteeringInput
-				NyaHookLib::Patch<uint16_t>(module + 0x484287, 0x9090);
-				NyaHookLib::Patch<uint8_t>(module + 0x4842A3, 0xEB);
+				NyaHookLib::Patch<uint16_t>(NyaHookLib::mEXEBase + 0x484287, 0x9090);
+				NyaHookLib::Patch<uint8_t>(NyaHookLib::mEXEBase + 0x4842A3, 0xEB);
 			}
 		} break;
 		default:
